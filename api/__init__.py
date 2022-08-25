197e4f0b62b009b87dcb38ab5a08aa1d1574aec5
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-08-25 09:24:28 +0900

    feat: 結果のキャッシュ

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 from fastapi import HTTPException
 from pixivpy3 import AppPixivAPI
@@ -24,19 +25,50 @@ def init_api():
 
 def get_illusts(word: str):
     api = init_api()
+    # キャッシュ
+    if not os.path.exists("/cache"):
+        os.mkdir("/cache")
+    if not os.path.exists("/cache/illusts"):
+        os.mkdir("/cache/illusts")
+
+    path = "/cache/illusts/" + word.replace(" ", "-") + ".json"
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            cache = json.load(f)
+            # 1時間以内のキャッシュか
+            if cache["updated_at"] > time.time() - 3600:
+                return cache["items"]
+
     # とりあえず90件取得
     items = []
     for i in range(3):
         results = api.search_illust(word, offset=i*30)
         if "illusts" not in results:
-            print(results)
             raise HTTPException(status_code=500)
         items.extend(results["illusts"])
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(json.dumps({"items": items, "updated_at": time.time()}))
+
     return items
 
 
 def get_novels(word: str):
     api = init_api()
+    # キャッシュ
+    if not os.path.exists("/cache"):
+        os.mkdir("/cache")
+    if not os.path.exists("/cache/novels"):
+        os.mkdir("/cache/novels")
+
+    path = "/cache/novels/" + word.replace(" ", "-") + ".json"
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            cache = json.load(f)
+            # 1時間以内のキャッシュか
+            if cache["updated_at"] > time.time() - 3600:
+                return cache["items"]
+
     # とりあえず90件取得
     items = []
     for i in range(3):
@@ -45,4 +77,8 @@ def get_novels(word: str):
             print(results)
             raise HTTPException(status_code=500)
         items.extend(results["novels"])
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(json.dumps({"items": items, "updated_at": time.time()}))
+
     return items
