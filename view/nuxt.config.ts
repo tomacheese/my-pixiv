1650799d23819d1c3d4a33b506a885cfde133c5b
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-09-08 22:02:18 +0900

    feat: favicon

--- view/nuxt.config.ts
+++ view/nuxt.config.ts
@@ -21,7 +21,36 @@ const config: NuxtConfig = {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicons/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/favicons/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/favicons/safari-pinned-tab.svg',
+        color: '#ffb41d',
+      },
+      {
+        rel: 'shortcut icon',
+        type: 'image/x-icon',
+        href: '/favicons/favicon.ico',
+      },
+    ],
   },
 
   css: ['@/assets/scroll.css'],
