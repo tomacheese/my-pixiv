e0c6a1d8d4fa18859778d890dfe87fbfcfdea772
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-10-07 13:01:00 +0900

    feat: #160 長押しのディレイを調整

--- view/src/plugins/vue-long-click.ts
+++ view/src/plugins/vue-long-click.ts
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import { longClickDirective } from 'vue-long-click'
-const longClickInstance = longClickDirective({ delay: 1000, interval: 500 })
+const longClickInstance = longClickDirective({ delay: 500, interval: 500 })
 Vue.directive('longclick', longClickInstance)
