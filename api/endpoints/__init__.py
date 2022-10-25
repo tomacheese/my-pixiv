c7225203379b15099585ff6a11fcf599dca9be83
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-10-25 14:03:45 +0900

    fix: 不要なエンドポイント削除

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.endpoints import images, like, settings_sync, tweet, ws
+from api.endpoints import images, settings_sync, ws
 
 router = APIRouter(prefix="/api")
 
@@ -12,6 +12,4 @@ def get_root():
 
 router.include_router(ws.router)
 router.include_router(images.router)
-router.include_router(like.router)
-router.include_router(tweet.router)
 router.include_router(settings_sync.router)
