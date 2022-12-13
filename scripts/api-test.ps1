65373c1b743d0e83f705ccd26a4b6978569b052a
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-14 05:20:22 +0900

    fix: remove api-test.ps1

--- scripts/api-test.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "stop"
-
-Set-Location $PSScriptRoot
-Set-Location ..
-
-. .\scripts\env.ps1
-
-$args = "workspace my-pixiv-api run test"
-Start-Process -FilePath yarn -ArgumentList $args -NoNewWindow -Wait
