7eb2c1c359838f7eff5fcc001a9f468b379344cd
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-15 02:42:07 +0900

    ci: タグ付けを固定にする (latestとtag)

--- scripts/client-dev.ps1
+++ scripts/client-dev.ps1
@@ -1,5 +1,4 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 
-Start-Process -FilePath yarn -WorkingDirectory "view" -NoNewWindow -Wait
 Start-Process -FilePath yarn -ArgumentList "dev" -WorkingDirectory "view" -NoNewWindow -Wait
