900e6f9f4db48246063ac561648b2d1f0a20ec79
Tomachi <8929706+book000@users.noreply.github.com>
2022-12-15 03:14:50 +0900

    ci: タグ付けを固定にする (latestとtag) (#369)

--- scripts/server-test.ps1
+++ scripts/server-test.ps1
@@ -5,5 +5,4 @@ Set-Location ..
 
 . .\scripts\env.ps1
 
-Start-Process -FilePath yarn -WorkingDirectory "api" -NoNewWindow -Wait
 Start-Process -FilePath yarn -ArgumentList "build-test" -WorkingDirectory "api" -NoNewWindow -Wait
