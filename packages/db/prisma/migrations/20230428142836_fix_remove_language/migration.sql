58f108e67b65eaf308d5ec5c552af5561cb84701
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-04-28 14:29:20 +0000

    fix(db-schema): remove language col

---
+++ packages/db/prisma/migrations/20230428142836_fix_remove_language/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `language` on the `novels` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "novels" DROP COLUMN "language";
